6d7754176fde74b392aca83362b751bf0df16d7f
yujinhong <yujinhong@hexindai.com>
2018-06-04 09:50:10 +0800

    Initial Commit

---
+++ YJHIdentityKeyBoard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YJHIdentityKeyBoard
+//
+//  Created by D on 2018/6/4.
+//  Copyright © 2018年 D. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YJHIdentityKeyBoard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YJHIdentityKeyBoard
+//
+//  Created by D on 2018/6/4.
+//  Copyright © 2018年 D. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
